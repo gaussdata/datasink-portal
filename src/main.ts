de93cd9c6ad1cf62566364ee435ae8fc483c3efe
gausszhou <gausszhou@qq.com>
2025-07-24 23:30:05 +0800

    fix: 在PV/UV数据请求中添加当前时间参数
    确保获取最新数据，避免缓存导致的数据不准确问题

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const BASE_URL = "/api/datasink/analysis";
 
 const fetchHour24 = async () => {
   const $div = document.getElementById("pvuv-hour") as HTMLCanvasElement;
-  const res = await fetch(BASE_URL + "/pvuv?date_level=hour").then((res) => res.json());
+  const res = await fetch(BASE_URL + `/pvuv?date_level=hour&end_time=${Date.now()}`).then((res) => res.json());
   const data = res?.data || [];
   data.forEach((item: any) => {
     item.x = item.date.slice(11, 13);
@@ -18,7 +18,7 @@ const fetchHour24 = async () => {
 
 const fetchDay30 = async () => {
   const $div = document.getElementById("pvuv-day") as HTMLCanvasElement;
-  const res = await fetch(BASE_URL + "/pvuv?date_level=day").then((res) => res.json());
+  const res = await fetch(BASE_URL + `/pvuv?date_level=day&end_time=${Date.now()}`).then((res) => res.json());
   const data = res?.data || [];
   data.forEach((item: any) => {
     item.x = item.date.slice(8, 10);
